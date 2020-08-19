c4390cb3802545d64c6b11a7e3ee207ec833e602
shidder <d.shidder@gmail.com>
2020-08-19 12:36:30 +0300

    2

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,6 @@
 var mainMenu = document.querySelector('.main-menu');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-// navMain.classList.remove('main-nav--nojs');
-
 navToggle.addEventListener('click', function () {
   if (mainMenu.classList.contains('main-menu--close')) {
     mainMenu.classList.remove('main-menu--close');
--- source/js/tabs.js
+++ source/js/tabs.js
@@ -14,74 +14,37 @@ var macedoniaCard = document.getElementById('macedonia-card');
 var montenegroCard = document.getElementById('montenegro-card');
 var croatiaCard = document.getElementById('croatia-card');
 
-
-
-
 tabButtons.onclick = function (evt) {
   var target = evt.target;
-  if(target === greeceButton) {
+  if (target === greeceButton) {
     greeceCard.classList.remove('card__wrapper--hidden')
-     albaniaCard.classList.add('card__wrapper--hidden')
-  macedoniaCard.classList.add('card__wrapper--hidden')
-   montenegroCard.classList.add('card__wrapper--hidden')
-   croatiaCard.classList.add('card__wrapper--hidden')
-  } else if(target === albaniaButton) {
+    albaniaCard.classList.add('card__wrapper--hidden')
+    macedoniaCard.classList.add('card__wrapper--hidden')
+    montenegroCard.classList.add('card__wrapper--hidden')
+    croatiaCard.classList.add('card__wrapper--hidden')
+  } else if (target === albaniaButton) {
     albaniaCard.classList.remove('card__wrapper--hidden')
-     greeceCard.classList.add('card__wrapper--hidden')
+    greeceCard.classList.add('card__wrapper--hidden')
     macedoniaCard.classList.add('card__wrapper--hidden')
-   montenegroCard.classList.add('card__wrapper--hidden')
-   croatiaCard.classList.add('card__wrapper--hidden')
-  } else if(target === macedoniaButton) {
+    montenegroCard.classList.add('card__wrapper--hidden')
+    croatiaCard.classList.add('card__wrapper--hidden')
+  } else if (target === macedoniaButton) {
     macedoniaCard.classList.remove('card__wrapper--hidden')
-     greeceCard.classList.add('card__wrapper--hidden')
+    greeceCard.classList.add('card__wrapper--hidden')
     albaniaCard.classList.add('card__wrapper--hidden')
-   montenegroCard.classList.add('card__wrapper--hidden')
-   croatiaCard.classList.add('card__wrapper--hidden')
-  } else if(target ===  montenegroButton) {
+    montenegroCard.classList.add('card__wrapper--hidden')
+    croatiaCard.classList.add('card__wrapper--hidden')
+  } else if (target === montenegroButton) {
     montenegroCard.classList.remove('card__wrapper--hidden')
-     greeceCard.classList.add('card__wrapper--hidden')
+    greeceCard.classList.add('card__wrapper--hidden')
     albaniaCard.classList.add('card__wrapper--hidden')
     macedoniaCard.classList.add('card__wrapper--hidden')
-   croatiaCard.classList.add('card__wrapper--hidden')
-  }else if(target === croatiaButton) {
+    croatiaCard.classList.add('card__wrapper--hidden')
+  } else if (target === croatiaButton) {
     croatiaCard.classList.remove('card__wrapper--hidden')
-     greeceCard.classList.add('card__wrapper--hidden')
-     albaniaCard.classList.add('card__wrapper--hidden')
+    greeceCard.classList.add('card__wrapper--hidden')
+    albaniaCard.classList.add('card__wrapper--hidden')
     macedoniaCard.classList.add('card__wrapper--hidden')
-   montenegroCard.classList.add('card__wrapper--hidden')
-
-  }
- }
-
-
-
-
-
-/*
-
-tabButtons.onclick =  function(event) {
-  var target = event.target;
-  switch (target) {
-    case greeceButton :
-    return greeceCard.classList.remove('card__wrapper--hidden');
-  case albaniaButton :
-    return albaniaCard.classList.remove('card__wrapper--hidden');
-}
-}
-*/
-var currentCard = document.getElementById('greece-card')
-var chooseCard = function (value) {
-  switch (currentCard) {
-    case albaniaButton :
-      return  albaniaCard.classList.remove('card__wrapper--hidden');
-    case macedoniaButton :
-      return macedoniaCard.classList.remove('card__wrapper--hidden');
-    case montenegroButton :
-      return montenegroCard.classList.remove('card__wrapper--hidden');
-    case croatiaButton:
-      croatiaCard.classList.remove('card__wrapper--hidden')
-    default:
-      return  greeceCard.classList.remove('card__wrapper--hidden');
+    montenegroCard.classList.add('card__wrapper--hidden')
   }
 }
-tabButtons.addEventListener('change', chooseCard);
